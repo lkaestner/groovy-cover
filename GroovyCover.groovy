0431baf58fe1dd5e0119829c12f86b7735efbfaa
Lukas Kästner <lkaestner@users.noreply.github.com>
2019-08-20 20:58:35 +0200

    added parameterizable CLI and changed slf4j-simple to logback

--- GroovyCover.groovy
+++ GroovyCover.groovy
@@ -2,84 +2,126 @@
 // META
 appName    = 'GroovyCover'
 appDescr   = 'Creates Album-Covers using ImageMagick'
-appVersion = '1.0.0'
+appVersion = '1.1.0'
 appAuthor  = 'Lukas Kästner'
 appLicense = 'SPDX: MIT'
 
 //=============================================================================
 // INIT
 
-// dependencies and imports:
-@Grab(group='org.slf4j', module='slf4j-simple', version='1.7.16')
-@Grab(group='com.google.guava', module='guava', version='27.1-jre')
+// grab dependencies
+@Grab(group='ch.qos.logback', module='logback-classic', version='1.2.3')
+@Grab(group='com.google.guava', module='guava', version='28.0-jre')
 
-import org.slf4j.impl.SimpleLogger
-import java.nio.file.Files
+// import classes
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.file.Files
 import com.google.common.io.MoreFiles
-
-//=============================================================================
-// SETTINGS
-
-final debug = false
-
-final inDir  = Paths.get('in')
-final outDir = Paths.get('out')
-
-final String textFont   = 'Arial-Bold'
-final String textColor  = 'white'
-final int outResolution = 1024
-final int outTextWidth  = outResolution * 0.7
-final int outTextHeight = outResolution * 0.2
-
-final allowedExtensions = ['jpg', 'jpeg', 'png']
 
 
 //=============================================================================
 // LOGGING
-System.properties[SimpleLogger.LOG_FILE_KEY] = 'System.out'
-System.properties[SimpleLogger.SHOW_THREAD_NAME_KEY] = 'false'
-System.properties[SimpleLogger.DEFAULT_LOG_LEVEL_KEY ] = debug ? 'debug' : 'info'
-final log = new SimpleLogger('')
-log.info("${appName} - ${java.time.LocalDateTime.now()}")
+final logger = org.slf4j.LoggerFactory.getLogger(appName)
+final encoder = new ch.qos.logback.classic.encoder.PatternLayoutEncoder()
+encoder.context = logger.loggerContext
+encoder.pattern = '%-5level - %msg%n'
+encoder.start()
+
+final appender = new ch.qos.logback.core.ConsoleAppender()
+appender.context = logger.loggerContext
+appender.encoder = encoder
+appender.start()
+
+logger.addAppender(appender)
+logger.additive = false
+logger.level = ch.qos.logback.classic.Level.INFO
+
+logger.info("${appName}")
+logger.info("Run this program with argument -h to view help and usage-information.")
+
+
+//=============================================================================
+// CLI ARGUMENTS
+
+// read and parse CLI arguments, thus specifying the public API
+final cli = new groovy.cli.commons.CliBuilder()
+
+cli.h(longOpt:'help', type: boolean, 'display help and usage-instructions')
+cli.d(longOpt:'debug', type: boolean, 'increase logging-verbosity and disable parallel file-processing')
+cli.i(longOpt:'in-dir', convert: { Paths.get(it) }, defaultValue: 'in', 'relative or absolute path of the input-directory')
+cli.o(longOpt:'out-dir', convert: { Paths.get(it) }, defaultValue: 'out', 'relative or absolute path of the output-directory')
+cli.f(longOpt:'text-font', type: String, defaultValue: 'Arial-Bold', 'name of the font, e.g. Arial-Bold')
+cli.c(longOpt:'text-color', type: String, defaultValue: 'white', 'color of the font, e.g. white')
+cli.r(longOpt:'out-resolution', type: int, defaultValue: '1024', 'resolution of output image (longest edge)')
+final options = cli.parse(args)
+
+// if requested, display help-text and exit
+if (options.'help') {
+	cli.usage()
+	System.exit(0)
+}
+
+// finalize CLI arguments and other constants
+final boolean debug          = options.'debug'
+final Path    inDir          = options.'in-dir'
+final Path    outDir         = options.'out-dir'
+final String  textColor      = options.'text-color'
+final String  textFont       = options.'text-font'
+final int     outResolution  = options.'out-resolution'
+final int     outTextWidth   = outResolution * 0.7
+final int     outTextHeight  = outResolution * 0.2
+final allowedExtensions      = ['jpg', 'jpeg', 'png']
 
 
 //=============================================================================
 // SCRIPT
 
-// create directories
-log.debug("Input directory: ${inDir.toAbsolutePath()}")
+// if requested, enable debugging and print basic debugging information
+if (debug) logger.level=ch.qos.logback.classic.Level.DEBUG
+logger.debug("Debugging is enabled")
+logger.debug("Date: ${java.time.LocalDateTime.now()}")
+logger.debug("Runtime: Groovy ${GroovySystem.version} on JDK ${System.properties['java.version']}")
+
+// ensure that input and output directories exist
+logger.debug("Input directory: ${inDir.toAbsolutePath()}")
 Files.createDirectories(inDir);
-log.debug("Output directory: ${outDir.toAbsolutePath()}")
+logger.debug("Output directory: ${outDir.toAbsolutePath()}")
 Files.createDirectories(outDir);
 
-// collect input files
-log.info('Determine list of input-files')
+// collect input files into a list (for logging and determining suitable degree of parallelization)
+logger.info('Determine list of input-files')
 final fileList = []
 inDir.eachFileRecurse(groovy.io.FileType.FILES) { currentFile ->
 	allowedExtensions.contains(MoreFiles.getFileExtension(currentFile)) && fileList << currentFile
 }
-fileList.empty && log.warn('No input files found.')
+fileList.empty && logger.warn('No input files found.')
 
 // process input-files in parallel
 numberOfThreads = debug ? 1 : Math.min(fileList.size(), Runtime.getRuntime().availableProcessors())
-log.info("Processing ${fileList.size()} input-files using ${numberOfThreads} parallel threads.")
+logger.info("Processing ${fileList.size()} input-files using ${numberOfThreads} parallel threads.")
 groovyx.gpars.GParsPool.withPool(numberOfThreads) {
 	fileList.eachParallel { inFile ->
 		final position = "[${fileList.indexOf(inFile)+1}/${fileList.size()}]"
-		log.info("${position}: ${inFile}")
+		logger.info("${position}: ${inFile}")
 		
 		// determine file-name without extension
 		final cleanName = MoreFiles.getNameWithoutExtension(inFile)
-		log.debug("Clean File-Name: ${cleanName}")
+		logger.debug("Clean File-Name: ${cleanName}")
 		
-		// determine output-file location
-		final outFile = outDir.resolve(inFile.fileName.toString());
-		log.debug("Output File: ${outFile}")
+		// determine file's relative path to the input-directory
+		final inFileRel = inDir.relativize(inFile);
+		
+		// determine output-file location, replicating the same relative path of the input-directory
+		final outFile = outDir.resolve(inFileRel);
+		logger.debug("Output File: ${outFile}")
+		
+		// prepare output directory
+		Files.createDirectories(outFile.parent)
 		Files.deleteIfExists(outFile)
 		
 		// build magick command
-		log.debug('Building Magick Command')
+		logger.debug('Building Magick Command')
 		final magickCommand = """
 		magick convert
 		-verbose
@@ -91,21 +133,20 @@ groovyx.gpars.GParsPool.withPool(numberOfThreads) {
 		"""
 		
 		// execute magick command
-		log.debug("Executing Magick Command:\n${magickCommand}")
+		logger.debug("Executing Magick Command:\n${magickCommand}")
 		final proc = magickCommand.execute()
 		proc.waitFor()
 		
 		// display execution result
 		final exitCode = proc.exitValue()
 		if (exitCode == 0) {
-			log.debug("Successfully created output-file: ${outFile}")
+			logger.debug("Successfully created output-file: ${outFile}")
 		} else {
-			log.error("""ExitCode: ${proc.exitValue()}
+			logger.error("""ExitCode: ${proc.exitValue()}
 						 StdErr:   ${proc.err.text}
 						 StdOut:   ${proc.in.text}""")
 		}
-
 	}
 }
 
-log.info('End')
+logger.info('End')
